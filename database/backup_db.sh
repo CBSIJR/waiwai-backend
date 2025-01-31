38f3bbd9aae04a8df3d75a5e519f0213e24fbc3b
Junior <user.emerson@outlook.com>
2025-01-31 12:59:06 -0300

    feat: cron backup do banco de dados

---
+++ database/backup_db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Configurações
+BACKUP_DIR="/backups"
+DATE=$(date +%Y%m%d%H%M%S)
+BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_${DATE}.sql"
+
+# Realizar o backup
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -d $POSTGRES_DB > $BACKUP_FILE
+
+# Verificar se o backup foi bem-sucedido
+if [ $? -eq 0 ]; then
+    echo "Backup realizado com sucesso: $BACKUP_FILE"
+    # Calcular o tamanho do arquivo de backup
+    BACKUP_SIZE=$(du -h $BACKUP_FILE | awk '{print $1}')
+    echo "Tamanho do backup: $BACKUP_SIZE"
+else
+    echo "Erro ao realizar o backup"
+    exit 1
+fi
