006deaa2fb257a3a52a8330884c84268718ab70b
aejuniordev <aejuniordev@icloud.com>
2025-11-04 08:32:43 -0300

    chore: inclui importação

--- backend/schemas/references.py
+++ backend/schemas/references.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from pydantic.networks import AnyHttpUrl, UrlConstraints
 
 from .base import Base
